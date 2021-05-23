d30e69534ba82220c9662b57e3dff1d6b4242695
Sethu Prakash <Sethuprakash25@gmail.com>
2021-05-23 16:17:17 +0530

    New
    new

---
+++ src/test/java/com/selenium/Seleniumwithjenkins.java
@@ -0,0 +1,25 @@
+package com.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Seleniumwithjenkins {
+
+    @Test(description = "TestSummary")
+    public void TitleCheck() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Programming\\SeleniumGitwithJenkins\\lib\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        Reporter.log("Browser is opened");
+        driver.get("https://www.amazon.in/");
+        String title = driver.getTitle();
+
+        Assert.assertEquals("Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in", title, "Incorrect");
+
+
+        driver.close();
+    }
+
+}
